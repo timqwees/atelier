3dd5ec70ac74375e5c531d362b9b3c686d2d9999
TimQwees <artemnersisyan777@gmail.com>
2026-05-23 15:46:16 +0300

    yandex

--- server.js
+++ server.js
@@ -839,6 +839,19 @@ function renderSeoPage(page, req) {
     <script defer src="/assets/voice-input.js"></script>
     <script defer src="/assets/lightbox.js"></script>
     <style>[data-testid="button-chat-toggle"] { display: none !important; }</style>
+    <!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109384423', 'ym');
+
+    ym(109384423, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/109384423" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
   </head>
   <body>
     <div id="root">${content}</div>

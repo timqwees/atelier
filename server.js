10c514838e8f440960e181979e4287593bbd1494
TimQwees <artemnersisyan777@gmail.com>
2026-06-15 12:05:09 +0300

    виджет

--- server.js
+++ server.js
@@ -338,6 +338,21 @@ function renderStaticFooter() {
     <meta itemprop="addressCountry" content="RU" />
   </div>
 </div>
+<script>
+  (function(d,t) {
+    var BASE_URL="https://app.chatwoot.com";
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=BASE_URL+"/packs/js/sdk.js";
+    g.async = true;
+    s.parentNode.insertBefore(g,s);
+    g.onload=function(){
+      window.chatwootSDK.run({
+        websiteToken: 'ZFDyf4j1nG7yALnV2ECbPG5H',
+        baseUrl: BASE_URL
+      })
+    }
+  })(document,"script");
+</script>
   `;
 }
 
